988b7ffadef6d26063b00e97976214174e9ecbd9
sezasada <111367099+sezasada@users.noreply.github.com>
2022-11-23 11:13:23 -0800

    Update part-supply.js
    done with question 7.

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -42,11 +42,22 @@ for( let x=0; x<supplyChanges.length; x++ ){
     }
 }
 
-
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
+for( let x of supplyChanges) {
+    console.log(x);
+    if( x > 0 ){
+        console.log( 'Added x parts.' );
+    }
+    if( x === 0 ){
+        console.log( 'No Change.' );
+    }
+    if( x < 0 ){
+        console.log( 'Removed x parts.' );
+    }
+}
 
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
