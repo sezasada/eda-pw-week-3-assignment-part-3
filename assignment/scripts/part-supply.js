f58d4237f7121ce9375b38015e49bec66de140b3
sezasada <111367099+sezasada@users.noreply.github.com>
2022-11-22 17:15:52 -0800

    Update part-supply.js
    done with #5

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -10,7 +10,7 @@ console.log(partsNeeded);
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('2. Array of supplyChanges:');
-let supplyChanges = [ 2, 5, -6, 0, 7, 11 ];
+let supplyChanges = [ 3, 5, -6, 0, 7, 11 ];
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('3. Second supplyChange is:');
 console.log(supplyChanges[1]);
@@ -23,7 +23,7 @@ console.log(supplyChanges);
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
-
+supplyChanges.push(25);
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
 //    - if it is a positive number (greater than 0), log 'Added x parts.' 
