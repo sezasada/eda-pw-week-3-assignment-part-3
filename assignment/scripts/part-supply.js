e8811cd4644461dcc6d56fe7372440d0f7cfeb31
sezasada <111367099+sezasada@users.noreply.github.com>
2022-11-23 17:16:09 -0800

    Update part-supply.js
    Almost done with 9.

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -76,11 +76,14 @@ console.log(sum);
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
-let max = 572;
-let parts = 0;
+let max = 572;;
 let box = 0;
-while(parts < max) {
-    parts += 7;
-    box++;
+while(max > 0) {
+    if( max / 7 >= 1) {
+        box++;
+    }
+    max -= 7;
 } 
 console.log(box);
+console.log(max);
+//567
