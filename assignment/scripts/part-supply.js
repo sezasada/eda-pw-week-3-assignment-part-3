871e3c71b275c4a80dae273b53f3b86ede72e997
sezasada <111367099+sezasada@users.noreply.github.com>
2022-11-30 11:33:06 -0800

    Update part-supply.js
    done.

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -30,17 +30,16 @@ supplyChanges.push(25);
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
 for( let x=0; x<supplyChanges.length; x++ ){
-    console.log( supplyChanges[ x ] );
-    if( supplyChanges[ x ] > 0 ){
-        console.log( 'Added x parts.' );
-    }
-    if( supplyChanges[ x ] === 0 ){
-        console.log( 'No Change.' );
-    }
-    if( supplyChanges[ x ] < 0 ){
-        console.log( 'Removed x parts.' );
-    }
-}
+    if( supplyChanges[x] > 0) {
+         console.log('Added', supplyChanges[x], 'parts');
+     }
+     if( supplyChanges[x] == 0) {
+         console.log('No change');
+     }
+     if( supplyChanges[x] < 0) {
+         console.log('Removed', supplyChanges[x], 'parts');
+     }
+ }
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
